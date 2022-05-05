2fd5c565364155c3f62b2267b9519ed115f11615
davidmyoungg <103277813+davidmyoungg@users.noreply.github.com>
2022-05-04 18:07:11 -0700

    update2

--- SkillDemoWk5_1Test.java
+++ SkillDemoWk5_1Test.java
@@ -4,7 +4,7 @@ import org.junit.*;
 public class SkillDemoWk5_1Test{
     @Test
     public void subtraction(){
-        assertEquals(2, SkillDemoWk5_1.subtract(4,3));
+        assertEquals(2, SkillDemoWk5_1.subtract(5,3)); //This test works!
     }
 
 }
